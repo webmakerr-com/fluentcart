5e000e232d282fbc5466f9299d345a3c8f0341ea
taskpul <romtietz@gmail.com>
2025-11-23 02:26:04 +0100

    Revert "Avoid rendering placeholder cards in related products"

--- fluent-cart/app/Services/Renderer/ProductCardRender.php
+++ fluent-cart/app/Services/Renderer/ProductCardRender.php
@@ -18,8 +18,6 @@ class ProductCardRender
 
     protected $cardVariant = 'default';
 
-    protected $imageData = [];
-
     public function __construct(Product $product, $config = [])
     {
         $this->product = $product;
@@ -44,12 +42,6 @@ class ProductCardRender
         $cursor = '';
         if (!empty($this->config['cursor'])) {
             $cursor = 'data-fluent-cart-cursor="' . esc_attr($this->config['cursor']) . '"';
-        }
-
-        $this->imageData = $this->prepareImageData();
-
-        if ($this->cardVariant === 'related' && Arr::get($this->imageData, 'is_placeholder')) {
-            return;
         }
 
         $cardWidth = '';
@@ -128,12 +120,19 @@ class ProductCardRender
 
     public function renderProductImage()
     {
-        if (!$this->imageData) {
-            $this->imageData = $this->prepareImageData();
+        $image = $this->product->thumbnail;
+        $isPlaceholder = false;
+
+        if (!$image) {
+            $image = Vite::getAssetUrl('images/placeholder.svg');
+            $isPlaceholder = true;
         }
 
-        $image = Arr::get($this->imageData, 'src');
-        $altText = Arr::get($this->imageData, 'alt_text');
+        $altText = $isPlaceholder
+                ? sprintf(
+                /* translators: %s: product title */
+                        __('Placeholder image for %s', 'fluent-cart'), $this->product->post_title)
+                : $this->product->post_title;
         ?>
         <a class="fct-product-card-image-wrap"
            href="<?php echo esc_url($this->viewUrl); ?>"
@@ -150,25 +149,6 @@ class ProductCardRender
                  height="270"/>
         </a>
         <?php
-    }
-
-    protected function prepareImageData()
-    {
-        $image = $this->product->thumbnail;
-        $placeholder = Vite::getAssetUrl('images/placeholder.svg');
-
-        $isPlaceholder = !$image || $image === $placeholder;
-        $imageSrc = $isPlaceholder ? $placeholder : $image;
-
-        return [
-            'src'            => $imageSrc,
-            'is_placeholder' => $isPlaceholder,
-            'alt_text'       => $isPlaceholder
-                ? sprintf(
-                    /* translators: %s: product title */
-                    __('Placeholder image for %s', 'fluent-cart'), $this->product->post_title)
-                : $this->product->post_title,
-        ];
     }
 
     public function renderPrices($wrapper_attributes = '')
